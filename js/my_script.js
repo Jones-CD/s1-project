2de5ba062643779ac5d98e4926eadc0c06bdb828
Chris <releasethebadgers@yahoo.co.uk>
2017-06-16 15:29:29 +0100

    Developing code

--- js/my_script.js
+++ js/my_script.js
@@ -1,37 +1,6 @@
 $(document).ready(function(){
 
-	//$("nav ul li").click(function(){  
-	//	$("nav ul li").addClass( "highlight-nav");
- 	//});
- 	$("#menu-icons").bind("click touchstart", function() {  //reveals menu, hides menu-icons
- 		$("nav").slideDown(1000);
- 		$("#menu-icons").hide();
- 	}); // closing tags menu reveal click
 
-
- 	//$("nav ul li").bind("click touchstart", function() {
- 		//$(this).addClass("highlight-nav");  //adds highlight to li
- 		//$("nav ul li".("highlight-nav").removeClass("highlight-nav");
-	//}); // closing tags menu item highlight click
-
- 	$("nav li.drop").bind("click touchstart", function() {   //reveals sub-menu items
- 		$(this).children().slideDown(1000);
- 	
-	});
-
- 	$("nav ul li").click(function(){  // on navbar----------------------------------------
-     	
-    	$(this).addClass("highlight-nav");
-    	$("nav ul li").removeClass("highlight-nav");
-    	
-    });
-
-//-----------------WHAT'S ON PAGE --------------------------------//
-
- 	$(".panel-heading").click(function() {   //trying to get 'down arrow' to disappear
- 	      $(this.downArrow).hide();  
-                        // and be replaced by 'up arrow'
-	});
 
 
 
@@ -42,9 +11,39 @@ $('.carousel').carousel({
   interval: 5000
 });
 
+// --------- FLIGHT BOOKING DATE/TIME SELECTION -------------------------------------------
 
+$(".dateSelect").click(function(e) {
 
+      var selectedDate = $(this).html();
+      $("#dropdownMenu1").html(selectedDate);
 
+      if (selectedDate === "Friday 12 Aug") {
+        $("#time6").html("17:00");
+        $("#time8").html("18:00");
+        $("#time10").html("19:00");
+        $("#time4").html("20:00");
+      }
+
+      if (selectedDate === "Saturday 13 Aug") {
+        $("#time1").html("06:00");
+        $("#time2").html("06.30");
+        $("#time3").html("07:00");
+        $("#time4").html("07:30");
+      }
+
+      if (selectedDate === "Sunday 14 Aug") {
+        $("#time1").html("06:00");
+        $("#time2").html("06.30");
+        $("#time3").html("07:00");
+        $("#time4").html("07:30");
+      }
+
+  });
+$(".timeSelect").click(function(e) {
+      var selectedTime = $(this).html();
+      $("#dropdownMenu2").html(selectedTime);
+});
 
 
 
