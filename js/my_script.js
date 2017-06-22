2be6286b90871f41cc8c5be41d9af17cef9dfda5
Chris <releasethebadgers@yahoo.co.uk>
2017-06-22 19:19:22 +0100

    Dev footer and carousel

--- js/my_script.js
+++ js/my_script.js
@@ -19,17 +19,17 @@ $(".dateSelect").click(function(e) {
       $("#dropdownMenu1").html(selectedDate);
 
       if (selectedDate === "Friday 12 Aug") {
-        $("#time6").html("17:00");
-        $("#time8").html("18:00");
-        $("#time10").html("19:00");
-        $("#time4").html("20:00");
+        $("#time2").html("18.00");
+        $("#time3").html("18:30");
+        $("#time1").html("19:00");
+        $("#time4").html("19:30");
       }
 
       if (selectedDate === "Saturday 13 Aug") {
         $("#time1").html("06:00");
         $("#time2").html("06.30");
-        $("#time3").html("07:00");
-        $("#time4").html("07:30");
+        $("#time3").html("18:30");
+        $("#time4").html("19:30");
       }
 
       if (selectedDate === "Sunday 14 Aug") {
