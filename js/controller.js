496a1fd478f8f2346dc445e9ea93c83eccdbf9ff
Chris <releasethebadgers@yahoo.co.uk>
2017-06-30 12:31:49 +0100

    Validation/tweaks

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,9 @@
 angular.module('myBBFApp', [])
     
-    .controller('BookingController', function($scope) 
+    .controller('bookingController', function($scope) 
  
-        $scope.submitForm = function() {
+        $scope.submitForm = function(){
             if ($scope.bookingForm.$valid) {
-                alert("Thank you for booking your balloon flight. We'll contact you shortly with everything you'll need to know.");
+                alert("Thank you for booking your balloon flight. We'll contact you shortly with everything you'll need to know.")
             }
     });
