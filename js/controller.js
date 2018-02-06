a325dde1ee5df2906aff16327602d8f7fe0b10ae
Chris <releasethebadgers@yahoo.co.uk>
2018-02-06 21:02:01 +0000

    Working on select fields

--- js/controller.js
+++ js/controller.js
@@ -17,6 +17,22 @@ angular.module('RouteControllers', [])
 				console.log(" in myOtherFunc");
 			}
   			
+      $scope.FriTimes = ["06.30", "07.30", "08.30"]
+ 
+      $scope.SatTimes = ["06.30", "07.30", "08.30", "19.30", "20.00", "20.30", "21.00"]
+      
+      $scope.SunTimes = ["06.30", "07.00", "07.30", "08.00", "08.30"]
+
+      
+
+      // This is my attempt to hide the (one to start with) alternate select fields once one has been chosen
+      $scope.pickFlightFunc = function () {
+        if ($scope.selectedTime.$valid) {
+            $(".sT1").addClass(".sT2-hide");
+
+        }
+      }
+
 	});
     	
    
